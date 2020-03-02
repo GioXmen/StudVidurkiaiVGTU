aaf3244c6dd1c4ee7aba62d4fd84c8ea4b2875b8
Ernestas <Ernestas.Lobacevskis@nortal.com>
2020-03-02 15:37:42 +0200

    Initial working build for v0.1

--- StudVidurkiaiVGTU/Program.cs
+++ StudVidurkiaiVGTU/Program.cs
@@ -1,12 +1,137 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StudVidurkiaiVGTU
 {
     class Program
     {
+        public static List<Studentas> studentai = new List<Studentas>();
+        public static StudentasArray  studentaiArray = new StudentasArray();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World2!");
+            Menu.ShowMenu();
+        }
+    }
+
+    internal class Menu
+    {
+        public static void ShowMenu()
+        {
+            Console.WriteLine("Studentų pažymiai v0.1 - Ernestas Lobačevskis");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("cl      - Console įvedimas su list");
+            Console.WriteLine("cm      - Console įvedimas su masyvais");
+            Console.WriteLine("rnd     - Įvestis sugeneruota");
+            Console.WriteLine("printv  - Spausdint vidurkius");
+            Console.WriteLine("printm  - Spausdint mediana");
+            Console.WriteLine("printv2 - Spausdint vidurkius [Array]");
+            Console.WriteLine("printm2 - Spausdint mediana [Array]");
+            Console.WriteLine("print   - Spausdint studentus");
+            Console.WriteLine("end     - Baigti programa");
+            Console.WriteLine("clear   - Valyti išvedimą");
+            Console.WriteLine("--------------------------------------------");
+
+            bool baigti = false;
+            while (baigti.Equals(false))
+            {
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine("Įveskite norimą veiksmą");
+                Console.WriteLine("--------------------------------------------");
+                switch (Console.ReadLine())
+                {
+                    case "cl":
+                        StudentData.ReadInputFromConsole();
+                        break;
+                    case "cm":
+                        StudentData.ReadInputFromConsoleArray();
+                        break;
+                    case "rnd":
+                        StudentData.ReadRandomInput();
+                        break;
+                    case "printv":
+                        PrintStudentsVidurkiai();
+                        break;
+                    case "printm":
+                        PrintStudentsMediana();
+                        break;
+                    case "printv2":
+                        PrintStudentsVidurkiaiM();
+                        break;
+                    case "printm2":
+                        PrintStudentsMedianaM();
+                        break;
+                    case "print":
+                        PrintStudents();
+                        break;
+                    case "end":
+                        baigti = true;
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Netinkamas variantas");
+                        break;
+                }
+            }
+        }
+        public static void PrintStudentsVidurkiai()
+        {
+            Console.WriteLine("{0, 5} {1, 15} {2, 20}", "Vardas", "Pavardė", "Galutinis (Vid.)");
+            Console.WriteLine("--------------------------------------------");
+            foreach (var studentas in Program.studentai)
+            {
+                Console.WriteLine("{0, 5} {1, 15} {2, 16}", studentas.Vardas, studentas.Pavarde, studentas.GetVidurkis());
+            }
+            Console.WriteLine("--------------------------------------------");
+        }
+        public static void PrintStudentsMediana()
+        {
+            Console.WriteLine("{0, 5} {1, 15} {2, 20}", "Vardas", "Pavardė", "Galutinis (Med.)");
+            Console.WriteLine("--------------------------------------------");
+            foreach (var studentas in Program.studentai)
+            {
+                Console.WriteLine("{0, 5} {1, 15} {2, 16}", studentas.Vardas, studentas.Pavarde, studentas.GetMediana());
+            }
+            Console.WriteLine("--------------------------------------------");
+        }
+
+        public static void PrintStudents()
+        {
+            foreach (var studentas in Program.studentai)
+            {
+                Console.WriteLine("-------------------------------------------------------------------------");
+                Console.WriteLine("Vardas: " + studentas.Vardas + "\n" + "Pavarde: " + studentas.Pavarde + "\n" +
+                    "NamuDarbai:" + string.Join("\t", studentas.NamuDarbai) + "\n" + "Egzaminas: " + studentas.Egzaminas + "\n" + 
+                    "Vidurkis: " + studentas.GetVidurkis() + "\n" + "Mediana: " + studentas.GetMediana());
+                Console.WriteLine("-------------------------------------------------------------------------");
+            }
+        }
+
+        public static void PrintStudentsVidurkiaiM()
+        {
+            Console.WriteLine("{0, 5} {1, 15} {2, 20}", "Vardas", "Pavardė", "Galutinis (Vid.)");
+            Console.WriteLine("--------------------------------------------");
+            for (int i = 0; i < Program.studentaiArray.GetArraySize(); i++)
+            {
+                Console.WriteLine("{0, 5} {1, 15} {2, 16}", Program.studentaiArray.vardas[i], Program.studentaiArray.pavarde[i],
+                    Program.studentaiArray.Vidurkis[i]);
+
+            }
+            Console.WriteLine("--------------------------------------------");
+        }
+        public static void PrintStudentsMedianaM()
+        {
+            Console.WriteLine("{0, 5} {1, 15} {2, 20}", "Vardas", "Pavardė", "Galutinis (Med.)");
+            Console.WriteLine("--------------------------------------------");
+            for (int i = 0; i < Program.studentaiArray.GetArraySize(); i++)
+            {
+                Console.WriteLine("{0, 5} {1, 15} {2, 16}", Program.studentaiArray.vardas[i], Program.studentaiArray.pavarde[i],
+                    Program.studentaiArray.Mediana[i].ToString("F2"));
+
+            }
+            Console.WriteLine("--------------------------------------------");
         }
     }
 }
---
+++ StudVidurkiaiVGTU/StudentData.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+
+namespace StudVidurkiaiVGTU
+{
+    class StudentData
+    {
+        private static readonly Random random = new Random();
+
+        public static void ReadInputFromConsole()
+        {
+            Studentas studentas = new Studentas();
+
+            Console.WriteLine("Įveskite studento duomenis:  ");
+            string duomenys = Console.ReadLine();
+            var splitData   = duomenys.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            studentas.Vardas  = splitData[0];
+            studentas.Pavarde = splitData[1];
+            for (int i = 2; i < splitData.Length - 1; i++)
+            {
+                studentas.NamuDarbai.Add(double.Parse(splitData[i]));
+            }
+            studentas.Egzaminas = double.Parse(splitData[splitData.Length - 1]);
+            Program.studentai.Add(studentas);
+
+            Console.WriteLine("Ar norite dar viena studenta prideti? [Y/N]:  ");
+
+            String answer = Console.ReadLine();
+            if (answer.Equals("Y") || answer.Equals("y"))
+            {
+                ReadInputFromConsole();
+            }
+        }
+        public static void ReadRandomInput()
+        {
+            Studentas studentas = new Studentas();
+            Random rnd = new Random();
+
+            studentas.Vardas  = GeneruotiVarda(9);
+            studentas.Pavarde = GeneruotiVarda(12);
+            for (int i = 0; i < 6; i++)
+            {
+                studentas.NamuDarbai.Add(rnd.Next(0, 10));
+            }
+            studentas.Egzaminas = rnd.Next(0, 10);
+            Program.studentai.Add(studentas);
+
+            Console.WriteLine(studentas.ToString());
+            Console.WriteLine("Ar norite dar viena studenta sugeneruoti? [Y/N]:  ");
+
+            String answer = Console.ReadLine();
+            if (answer.Equals("Y") || answer.Equals("y"))
+            {
+                ReadRandomInput();
+            }
+        }
+        private static string GeneruotiVarda(int ilgis)
+        {
+            const string abc = "abcdefghijklmnopqrstuvwxyz";
+            return new string(Enumerable.Repeat(abc, ilgis)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static void ReadInputFromConsoleArray()
+        {
+            int studentoNr = Program.studentaiArray.GetArraySize();
+
+            Console.WriteLine("Įveskite studento duomenis:  ");
+            string duomenys = Console.ReadLine();
+            var splitData   = duomenys.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Program.studentaiArray.vardas[studentoNr] = splitData[0];
+            Program.studentaiArray.pavarde[studentoNr] = splitData[1];
+
+            for (int i = 2; i < splitData.Length - 1; i++)
+            {
+                Program.studentaiArray.NamuDarbai[studentoNr,i-2] = double.Parse(splitData[i]);
+            }
+
+            Program.studentaiArray.egzaminas[studentoNr] = double.Parse(splitData[splitData.Length - 1]);
+            Program.studentaiArray.SkaiciuotiVidurki(studentoNr);
+            Program.studentaiArray.SkaiciuotiMediana(studentoNr);
+            Program.studentaiArray.dydis += 1;
+
+            Console.WriteLine("Ar norite dar viena studenta prideti? [Y/N]:  ");
+
+            String answer = Console.ReadLine();
+            if (answer.Equals("Y") || answer.Equals("y"))
+            {
+                ReadInputFromConsoleArray();
+            }
+        }
+    }
+}
